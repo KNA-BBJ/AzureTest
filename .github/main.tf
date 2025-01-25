94b38cf8023b6389f06bccb37bdc3d576ee981d4
KNA-BBJ <53668990+KNA-BBJ@users.noreply.github.com>
2025-01-25 23:21:05 +1000

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -1,20 +1,8 @@
-# Provider Configuration
 provider "azurerm" {
   features {}
 }
 
-# Define the Resource Group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
-
-# Outputs
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
-}
-
-output "resource_group_location" {
-  value = azurerm_resource_group.example.location
-}
-
