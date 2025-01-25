bb6e029ecac71f7fc75c58321617e1d4e19a37e8
Barrie.Russell@gmail.com <barrie.russell@gmail.com>
2025-01-26 08:28:09 +1000

    fdgggg

--- .github/main.tf
+++ .github/main.tf
@@ -1,8 +1,22 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_version = ">=1.6.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"      
+    }    
+  }
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example1-resources"
-  location = "East US"
+provider "azurerm" {
+  features {    
+  }
+}
+resource "azurerm_storage_account" "iac_stg_acct" {
+  name                     = "iacstgacct20250225xzy"
+  resource_group_name      = "DefaultResourceGroup-EUS"
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
