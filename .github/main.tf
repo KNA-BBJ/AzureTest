3fcaee317b1900712708a71bc63defb81b4f9fea
KNA-BBJ <53668990+KNA-BBJ@users.noreply.github.com>
2025-01-25 23:16:23 +1000

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -1,8 +1,20 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-backend-rg"
-    storage_account_name = "terraformstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+# Provider Configuration
+provider "azurerm" {
+  features {}
 }
+
+# Define the Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.example.location
+}
+
