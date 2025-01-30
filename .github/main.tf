bc283e8e077abb74f3d689396e317ade31cf15f2
KNA-BBJ <53668990+KNA-BBJ@users.noreply.github.com>
2025-01-30 19:53:01 +1000

    Test update

--- .github/main.tf
+++ .github/main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 resource "azurerm_storage_account" "iac_stg_acct" {
   name                     = "iacstgacct20250225xzy"
-  resource_group_name      = "DefaultResourceGroup-EUS"
+  resource_group_name      = "MojoBongo"
   location                 = "eastus"
   account_tier             = "Standard"
   account_replication_type = "LRS"
